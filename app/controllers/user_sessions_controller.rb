16e290ccbfc7409e7fd6f810bfaaad1905b52f73
megu <yasuimegumi@yasuimeguminoMacBook-Air.local>
2024-05-16 16:40:16 +0900

    add:マイページを追加しました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,10 +7,14 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path
+      redirect_to mypage_path
     else
       render :new
     end
+  end
+
+  def mypage
+    @user = current_user
   end
 
   def destroy
