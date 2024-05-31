fe347aaf17f0acd1215a3b446ce0bef27c7b5985
megu <yasuimegumi@yasuimeguminoMacBook-Air.local>
2024-05-31 17:01:13 +0900

    add:お気に入り機能を作成しました

--- app/models/hiragana.rb
+++ app/models/hiragana.rb
@@ -5,4 +5,7 @@ class Hiragana < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sign_language
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_by_users, through: :favorites, source: :user
 end
