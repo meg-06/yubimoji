03a3c96662abe2a9047dcee271bb614d28f0279a
megu <yasuimegumi@yasuimeguminoMacBook-Air.local>
2024-06-25 13:29:17 +0900

    update:ログイン前でも利用規約とプライバシーポリシーを確認できるようにしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :danger
+  skip_before_action :require_login, if: :high_voltage_page?
 
   private
 
   def not_authenticated
     redirect_to login_path
+  end
+
+  def high_voltage_page?
+    controller_path =~ /^high_voltage\/pages/
   end
 end
