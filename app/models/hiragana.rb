654f3e04232a9f59f8df775056a0114e17f72193
megu <yasuimegumi@yasuimeguminoMacBook-Air.local>
2024-06-29 16:58:19 +0900

    update:「ゔ」を入力できないようにしました

--- app/models/hiragana.rb
+++ app/models/hiragana.rb
@@ -2,6 +2,7 @@ class Hiragana < ApplicationRecord
   validates :character, presence: { message: '入力してください' }
   validates :character, uniqueness: { scope: :user_id, message: 'この単語は既に登録されています' }, if: -> { character.present? }
   validates :character, format: { with: /\A[\p{hiragana}ー－]+\z/, message: 'ひらがなで入力してください' }, if: -> { character.present? }
+  validate :no_vu_character
 
   belongs_to :user
 
@@ -10,4 +11,10 @@ class Hiragana < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :favorited_by_users, through: :favorites, source: :user
+
+  def no_vu_character
+    if character&.include?('ゔ')
+      errors.add(:character, '「ゔ」を含めることはできません')
+    end
+  end
 end
